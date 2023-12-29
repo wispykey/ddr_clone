b40b8efd03f92c52ae870e399e0748e3eadeb4e1
wispykey <hui.keelan@gmail.com>
2023-12-28 21:47:42 -0800

    Improved sync issues

--- main.gd
+++ main.gd
@@ -7,6 +7,8 @@ var BPM: float
 var TIME_NUMERATOR: float
 var QUARTER_NOTES_PER_SECOND: float
 var QUARTER_NOTE_DURATION: float
+var MEASURE_DURATION: float
+var FALL_DURATION: float
 var music_has_intro
 
 var PERFECT_TIMING_Y: float
@@ -14,26 +16,29 @@ var PERFECT_POINTS: float = 1000
 var GOOD_POINTS: float = 800
 var OK_POINTS: float = 500
 var MAX_SCORE: float
+var MAX_COMBO
 
-var LOADING_DELAY_OFFSET: float = 0.014
+var LOADING_DELAY_OFFSET: float = 0.1
 
 var game_has_started = false
 var play_countdown_sfx = false
 
+var last_beat: float = 0.0
+
 # Obviously this is hard-coded for now...
 var arrow_timers: Array = [
-	["right",2],["right",1],["down",1],
-	["left",2],["right",1],["left",1],
-	["left",2],["down",1],["down",1],
-	["left",2],["right",1],["right",1],
-	["left",2],["right",1],["left",1],
-	["left",2],["right",1],["left",1],
-	["left",1],["down",1],["right",1],["left",1],
-	["left",2],["right",1],["left",1],
-	["left",2],["left",1],["down",1],
-	["right",2],["left",1],["down",1],
-	["right",2]]
-var arrow_timers_index = 0
+	["right",0],["right",2],["down",3],
+	["left",4],["right",6],["left",7],
+	["left",8],["down",10],["down",11],
+	["left",12],["right",14],["right",15],
+	["left",16],["right",18],["left",19],
+	["left",20],["right",22],["left",23],
+	["left",24],["down",25],["right",26],["left",27],
+	["left",28],["right",30],["left",31],
+	["left",32],["left",34],["down",35],
+	["right",36],["left",38],["down",39],
+	["right",40]]
+var arrows_before_music: Array = []
 
 var curr_combo = 0
 var max_combo = 0
@@ -41,15 +46,15 @@ var score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$ArrowTimer.timeout.connect(_on_arrow_timer_timeout)
 	$StartTimer.timeout.connect(_on_start_timer_timeout)
 	$EndTimer.timeout.connect(_on_end_timer_timeout)
 	$MusicStartTimer.timeout.connect(_on_music_start_timer_timeout)
+	$Conductor.beat.connect(_on_conductor_beat)
 	$Player.hit.connect(_on_player_hit)
 	$Player.miss.connect(_on_player_miss)
-	
+	$HUD/GameOverMenu/RetryButton.button_down.connect(_on_retry_button_button_down)
+
 	#TODO: Change these to be parsed from specific level data
-	$ArrowTimer.wait_time = 0 # units, not seconds
 	$EndTimer.wait_time = 40 # no need to convert
 	music_has_intro = false
 	
@@ -57,34 +62,57 @@ func _ready() -> void:
 	TIME_NUMERATOR = 4
 	QUARTER_NOTES_PER_SECOND = BPM / 60
 	QUARTER_NOTE_DURATION = 60 / BPM
+	MEASURE_DURATION = TIME_NUMERATOR * QUARTER_NOTE_DURATION
 	
 	if music_has_intro:
-		$ArrowTimer.wait_time /= QUARTER_NOTES_PER_SECOND
-		print($ArrowTimer.wait_time)
+		pass
 	
 	PERFECT_TIMING_Y = get_node("Player/LeftInputHitbox/CollisionShape2D").position.y
+	
+	MAX_COMBO = arrow_timers.size()
 	MAX_SCORE = PERFECT_POINTS * arrow_timers.size()
+	
+	FALL_DURATION = (PERFECT_TIMING_Y + 60) / FALL_SPEED
+	
+	compute_offset_beat_times()
 	
 	new_game()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if !game_has_started:
-		if $MusicStartTimer.time_left <= TIME_NUMERATOR * QUARTER_NOTE_DURATION:
-			var countdown_text = str(ceil($MusicStartTimer.time_left / QUARTER_NOTE_DURATION) - 1)
+		if $MusicStartTimer.time_left < TIME_NUMERATOR * QUARTER_NOTE_DURATION:
+			var countdown_text = str(max(ceil($MusicStartTimer.time_left / QUARTER_NOTE_DURATION) - 1,0))
 			if countdown_text == "0":
 				countdown_text = "GO!"
 			if countdown_text != $HUD/CountdownLabel.text:
+				print(countdown_text)
 				if countdown_text == "GO!":
 					$CountdownPlayer.stream = load("res://sound/countdown_high.wav")
+				
 				$CountdownPlayer.playing = true
 			$HUD/CountdownLabel.text = countdown_text
 			if $MusicStartTimer.time_left == 0:
 				game_has_started = true
-				$HUD/CountdownLabel.text = ""
-	$HUD/ScoreLabel.text = str(score)
+				$HUD/CountdownLabel.visible = false
+				
+	if ($Conductor.song_position > last_beat + QUARTER_NOTE_DURATION):
+		last_beat += QUARTER_NOTE_DURATION
 
+	if (!arrow_timers.is_empty()):
+		if arrow_timers[0][1] <= $Conductor.song_position && arrow_timers[0][1] >= 0:
+			var arrow = arrow_timers.pop_front()
+			spawn_arrow(arrow)
+			
+	if (!arrows_before_music.is_empty()):
+		if abs(arrows_before_music[0][1]) >= $MusicStartTimer.time_left:
+			var arrow = arrows_before_music.pop_front()
+			spawn_arrow(arrow)
 	
+	$HUD/ScoreLabel.text = str(score)
+	$HUD/ComboLabel.text = str(curr_combo) + "x"
+
+		
 func new_game():
 	$StartTimer.start()
 	
@@ -101,7 +129,11 @@ func game_over():
 		$HUD/GameOverMenu/FinalRankLabel.text = "Rank C"
 	else:
 		$HUD/GameOverMenu/FinalRankLabel.text = "Rank D"
-		
+	
+	if max_combo == MAX_COMBO:
+		$HUD/GameOverMenu/ComboLabel.text = "Full Combo!"
+	else:
+		$HUD/GameOverMenu/ComboLabel.text = "Max Combo:\n" + str(max_combo)
 	$HUD/GameOverMenu.visible = true
 	#TODO: Show exit and play-again buttons.
 
@@ -112,61 +144,44 @@ func _on_start_timer_timeout() -> void:
 	var countdown_delay = QUARTER_NOTE_DURATION * TIME_NUMERATOR
 
 	if (countdown_delay < first_arrow_delay):
-		$ArrowTimer.wait_time = 0.00001
+		# $ArrowTimer.wait_time = 0.00001
 		$MusicStartTimer.wait_time = first_arrow_delay
 	else:
-		$ArrowTimer.wait_time = countdown_delay - first_arrow_delay
+		# $ArrowTimer.wait_time = countdown_delay - first_arrow_delay
 		$MusicStartTimer.wait_time = countdown_delay
 	
-	$ArrowTimer.start()
 	$MusicStartTimer.start()
 	
 func _on_end_timer_timeout() -> void:
 	game_over()
 	
 func _on_music_start_timer_timeout() -> void:
-	$MusicPlayer.playing = true
+	$Conductor.playing = true
 
-func _on_arrow_timer_timeout() -> void:
-	# No more arrows to load
-	if arrow_timers_index >= arrow_timers.size():
-		return
+func _on_conductor_beat(last_reported_beat) -> void:
+	pass
 	
-	var arrow = arrow_scene.instantiate()
+func spawn_arrow(arrow):
+		
+	var arrow_scene = arrow_scene.instantiate()
 	
 	# Spawn arrow left, middle, or right
 	var arrow_spawn_location
-	if arrow_timers[arrow_timers_index][0] == "left":
+	if arrow[0] == "left":
 		arrow_spawn_location = get_node("LeftArrowPath/LeftArrowSpawnLocation")
-		arrow.rotation -= PI / 2 
-	elif arrow_timers[arrow_timers_index][0] == "down":
+		arrow_scene.rotation -= PI / 2 
+	elif arrow[0] == "down":
 		arrow_spawn_location = get_node("DownArrowPath/DownArrowSpawnLocation")
-		arrow.rotation += PI
-	elif arrow_timers[arrow_timers_index][0] == "right":
+		arrow_scene.rotation += PI
+	elif arrow[0] == "right":
 		arrow_spawn_location = get_node("RightArrowPath/RightArrowSpawnLocation")
-		arrow.rotation += PI / 2
-	arrow.position = arrow_spawn_location.position
+		arrow_scene.rotation += PI / 2
+	arrow_scene.position = arrow_spawn_location.position
 
 	var velocity = Vector2.DOWN * FALL_SPEED
-	arrow.linear_velocity = velocity
+	arrow_scene.linear_velocity = velocity
 
-	add_child(arrow)
-	
-	# Get, set and start the timer for next arrow
-	
-	# Static version for testing
-	'''
-	var arrow_timer = get_node("ArrowTimer") as Timer
-	arrow_timer.wait_time = 0.2
-	arrow_timer.start()
-	'''
-	
-	# Enumerated version
-	var arrow_timer = get_node("ArrowTimer") as Timer
-	arrow_timer.wait_time = arrow_timers[arrow_timers_index][1] / QUARTER_NOTES_PER_SECOND
-	arrow_timer.wait_time -= LOADING_DELAY_OFFSET
-	arrow_timer.start()
-	arrow_timers_index += 1
+	add_child(arrow_scene)
 
 
 func _on_player_hit(body) -> void:
@@ -196,3 +211,19 @@ func update_score_and_play_sfx(body: RigidBody2D):
 func update_combo():
 	curr_combo += 1
 	max_combo = max(curr_combo, max_combo)
+	
+func compute_offset_beat_times():
+	for i in range(arrow_timers.size()):
+		arrow_timers[i][1] = (arrow_timers[i][1] * (60 / BPM)) - FALL_DURATION - LOADING_DELAY_OFFSET
+		
+	var num_negative = 0
+	for i in range(arrow_timers.size()):
+		if arrow_timers[i][1] < 0:
+			arrows_before_music.append(arrow_timers[i])
+			num_negative += 1
+			
+	arrow_timers = arrow_timers.slice(num_negative, arrow_timers.size())
+
+func _on_retry_button_button_down():
+	print("pressed")
+	get_tree().reload_current_scene()
